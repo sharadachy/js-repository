d29c325533952eec5d09b6c0e174ed23746c8d82
Sharada <108538819+sharadachy@users.noreply.github.com>
2025-06-08 08:02:47 +0000

    config

--- basicJS/01_basic.js
+++ basicJS/01_basic.js
@@ -1,3 +1,5 @@
 const accountID = 12345
 let accuntEmail = "skc@gmail.com"
 console.log([accountID, accuntEmail]);
+//we should not use var datatype in js because it doesnot support 
+//scope and functional scope.
